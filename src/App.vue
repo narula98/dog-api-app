d1945e6d7cee9e86f4dc2c846c9a07a7758a58d3
narula98 <sahilnarula98@gmail.com>
2022-04-01 11:02:11 +0530

    Project Setup done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DogList/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DogList from './components/DogList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DogList,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="sass">
+  @import './app.scss'
+</style>>
